e81bad0d9441e53c4fe87dabd909555f601340bb
R2D2 <r2d2@MacBook-Pro-R2D2.local>
2023-09-12 13:38:23 +0600

    here just register and login

---
+++ app/src/main/java/com/example/easyshelf/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.easyshelf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = auth.getCurrentUser();
+
+        if (currentUser == null){
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+            return;
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/easyshelf/User.java
@@ -0,0 +1,12 @@
+package com.example.easyshelf;
+
+public class User {
+    public String email;
+
+    public User() {
+    }
+
+    public User (String email){
+        this.email = email;
+    }
+}
