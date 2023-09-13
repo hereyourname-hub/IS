de10fc542b2a5cbd3e0c029228060dd76f160e67
r2d2 <nurlnbekova@adam.edu.kg>
2023-09-13 13:26:20 +0600

    logo

---
+++ app/src/main/java/com/example/easyshelf/Frame18.java
@@ -0,0 +1,14 @@
+package com.example.easyshelf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Frame18 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frame18);
+    }
+}
