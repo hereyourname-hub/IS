ae0fc90cd277ae3f2ff2686ec023dc6d0966059c
r2d2 <nurlnbekova@adam.edu.kg>
2023-09-17 01:00:23 +0600

    here pr24

--- app/src/main/java/com/example/easyshelf/Frame24.java
+++ app/src/main/java/com/example/easyshelf/Frame24.java
@@ -1,14 +1,44 @@
 package com.example.easyshelf;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.easyshelf.R;
 
 public class Frame24 extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frame24);
+
+        ImageButton btnForShelf = findViewById(R.id.btn_for_shelf);
+
+        btnForShelf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Создаем AlertDialog
+                AlertDialog.Builder builder = new AlertDialog.Builder(Frame24.this);
+
+                // Устанавливаем макет для диалогового окна с фоном-обводкой
+                View dialogView = getLayoutInflater().inflate(R.layout.dialog_layout, null);
+                builder.setView(dialogView);
+
+
+                // Устанавливаем кнопку "Закрыть"
+                builder.setPositiveButton("Закрыть", null);
+
+                // Показываем диалоговое окно
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+                // Устанавливаем фон-обводку для диалогового окна
+                dialog.getWindow().setBackgroundDrawableResource(R.drawable.bg_button24);
+            }
+        });
     }
 }
